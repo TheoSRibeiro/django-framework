5912373ffca93aa9faed38821843fa3a03972f32
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-09 09:30:33 -0300

    Paginacao com DRF

--- cursos/models.py
+++ cursos/models.py
@@ -19,6 +19,7 @@ class Curso(Base):
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
+        ordering = ['id']
 
     def __str__(self):
         return self.titulo
@@ -36,6 +37,7 @@ class Avaliacao(Base):
         verbose_name = 'Avaliação'
         verbose_name_plural = 'Avaliações'
         unique_together = ['email', 'curso'] #somente 1 pessoa com o mesmo email pode avaliar, uma vez, o curso
+        ordering = ['id']
 
     def __str__(self):
         return f'{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}'
